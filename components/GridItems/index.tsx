f593bd9104b27b8c76d665d52f3ebf3ef1e3c036
YangTuanAnh <yangtuananh2003@gmail.com>
2023-01-28 14:03:53 +0700

    change: deployment issues

--- components/GridItems/index.tsx
+++ components/GridItems/index.tsx
@@ -4,7 +4,7 @@ import Image, { StaticImageData } from "next/image";
 const GridItem = ({children, href, title, thumbnail}:{children:any, href:string, title:string, thumbnail:StaticImageData}) => {
     return (
         <div className="w-full text-center">
-            <a href={href} target="_blank" className="hover:text-highlight transition duration-150">
+            <a href={href} target="_blank" rel="noreferrer" className="hover:text-highlight transition duration-150">
                 <Image  src={thumbnail}
                         alt={title}
                         placeholder="blur"
