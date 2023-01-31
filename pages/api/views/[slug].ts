1b0533165aad484ba5d09baf3511aef689b33890
YangTuanAnh <yangtuananh2003@gmail.com>
2023-01-31 19:47:26 +0700

    add: timestamp tracking for blog post views

--- pages/api/views/[slug].ts
+++ pages/api/views/[slug].ts
@@ -1,16 +1,25 @@
 import db from '../../../lib/firebase'
 
-export default async (req : any, res : any) => {
+export default async (req: any, res: any) =>
+{
   // increment the views
-  if (req.method === 'POST') {
+  if (req.method === 'POST')
+  {
     const ref = db.ref('views').child(req.query.slug)
-    const { snapshot } = await ref.transaction((currentViews) => {
-      if (currentViews === null) {
+    const { snapshot } = await ref.transaction((currentViews) =>
+    {
+      if (currentViews === null)
+      {
         return 1
       }
 
       return currentViews + 1
     })
+
+    db.ref("viewTime")
+      .child(req.query.slug)
+        .child(new Date().toLocaleString().replace(/\//g, '-'))
+          .set(snapshot.val())
 
     return res.status(200).json({
       total: snapshot.val(),
@@ -18,7 +27,8 @@ export default async (req : any, res : any) => {
   }
 
   // fetch the views
-  if (req.method === 'GET') {
+  if (req.method === 'GET')
+  {
     const snapshot = await db.ref('views').child(req.query.slug).once('value')
     const views = snapshot.val()
 
