8112004bdbf38e72e5214e451e7f10f4ba25bce1
YangTuanAnh <yangtuananh2003@gmail.com>
2023-12-24 18:02:41 +0700

    add: kofi button

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const kofi = `<script>
+  kofiWidgetOverlay.draw('yangtuananh', {
+    'type': 'floating-chat',
+  'floating-chat.donateButton.text': 'Support me',
+  'floating-chat.donateButton.background-color': '#00b9fe',
+  'floating-chat.donateButton.text-color': '#fff'
+});
+</script>`
   return (
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Manrope"></link>
+        <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
       </Head>
       <body>
         <Main />
         <NextScript />
+        <div dangerouslySetInnerHTML={{ __html: kofi }} />
       </body>
     </Html>
   )
