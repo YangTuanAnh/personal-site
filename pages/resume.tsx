e2affaf4a5305db28172ec4b7e226bc20e01d0b4
Yang3101 <3101@bvm.network>
2024-07-26 03:52:57 +0700

    add: new resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -2,12 +2,12 @@ const Resume = () =>
 {
     return (
         <div className="text-center">
-            <a href="YangTuanAnh_SWEResume_2024.pdf" download>
+            <a href="YangTuanAnh_English_CV_Academic.pdf" download>
                 <button className="transition ease-in-out hover:-translate-y-1 hover:scale-110 bg-amber-400 hover:brightness-110 text-neutral-900 inline-flex px-4 py-2 my-6 rounded-lg items-center">
                     Download
                 </button>
             </a>
-            <iframe src="YangTuanAnh_SWEResume_2024.pdf" width="100%" height="800px" />
+            <iframe src="YangTuanAnh_English_CV_Academic.pdf" width="100%" height="800px" />
         </div>
     )
 }
